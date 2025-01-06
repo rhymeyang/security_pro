b61d15853a5243491c6444a539f42bf47c97b29a
rhyme_yang <rhyme_yang@live.cn>
2025-01-06 14:42:48 -0500

    back

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -146,8 +146,12 @@ export default defineUserConfig({
             '/09/09.02.VirtualNetworking.md',
             '/09/09.03.SoftwareDefinedNetworking.md',
             '/09/09.04.CloudServices.md',
-            '/09/09.05.CloudSecurity.md'
-            
+            '/09/09.05.CloudSecurity.md',
+            '/09/',
+            '/09/',
+            '/09/',
+            '/09/',
+            '/10/'
           ],
         },
         {
